77d16adabfcc42bfbef5ebc8da3110abb1d3c453
Rafael Cardona <31547586+rcardona-os@users.noreply.github.com>
2018-05-29 11:39:08 +0200

    Update checkingAnsibleEmbeddedJob.sh

--- checkingAnsibleEmbeddedJob.sh
+++ checkingAnsibleEmbeddedJob.sh
@@ -2,4 +2,10 @@
 
 MYLOG=`ls -ltr /var/lib/awx/job_status | awk -F' ' '{print $9}'| tail -1`
 
-echo /var/lib/awx/job_status/$MYLOG
+echo "  "
+echo " Current time : `date| awk -F' ' '{print $2 " " $3 " " $4}'` "
+echo " Logfile time : `ls -ltr /var/lib/awx/job_status | awk -F' ' '{print $6 " " $7 " " $8}' | tail -1`"
+echo " Last logfile : `ls -ltr /var/lib/awx/job_status | awk -F' ' '{print $9}' | tail -1`"
+echo "   "
+tailf /var/lib/awx/job_status/$MYLOG
+echo "   "
